1dbf137c283b019c6b9f010594fe1e256fef26be
ArianSdg <ariansadegh8305@gmail.com>
2024-05-03 18:15:21 +0430

    Fixed a minor mistake in the constructor

--- src/Item.java
+++ src/Item.java
@@ -3,11 +3,14 @@ public class Item {
     private int count;
     private String description;
     Item (Food food, int count) {
+        this.food = food;
         this.count = count;
         description = "-";
     }
     Item (Food food, int count, String description) {
         this.count = count;
+        this.food = food;
+        this.description = description;
     }
 
     public String getDescription() {
