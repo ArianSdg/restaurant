2a5939835724cf03fa234033828bbfc11dc63f09
ArianSdg <ariansadegh8305@gmail.com>
2024-05-03 16:50:35 +0430

    changing arrays to vectors

--- src/Food.java
+++ src/Food.java
@@ -1,34 +1,22 @@
+import java.util.Vector;
+
 public class Food {
     private String name;
     private int price;
-    static int counter = 0;
-    private static String[] menu = new String[0];
+    private static Vector<Food> menu = new Vector<>();
+
     Food (String name, int price) {
         this.name = name;
         this.price = price;
-        menu = increaseSize(menu);
-        menu[counter] = name;
-        counter++;
+        menu.add(this);
     }
-    public void setName(String newName) {
-        this.name = newName;
+    public static Vector<Food> getMenu() {
+        return menu;
     }
     public String getName() {
         return name;
     }
-    public void setPrice(int newPrice) {
-        this.price = newPrice;
-    }
     public int getPrice() {
         return price;
-    }
-    public static String[] getMenu() {
-        return menu;
-    }
-    public String[] increaseSize(String[] menu) {
-        String[] temp = new String[menu.length + 1];
-
-        System.arraycopy(menu, 0, temp, 0, menu.length);
-        return temp;
     }
 }
