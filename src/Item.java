55de42662e1d7216e73d9dd2dabd13c5e597bb55
ArianSdg <ariansadegh8305@gmail.com>
2024-05-03 17:15:39 +0430

    .

---
+++ src/Item.java
@@ -0,0 +1,21 @@
+public class Item {
+    private Food food;
+    private int count;
+    private String description;
+    Item (Food food, int count) {
+        this.count = count;
+        description = "-";
+    }
+    Item (Food food, int count, String description) {
+        this.count = count;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public int getCount() {
+        return count;
+    }
+    public Food getFood() {
+        return food;
+    }
+}
